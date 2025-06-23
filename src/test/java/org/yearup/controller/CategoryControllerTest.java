9c775415842399c2d1d874ba66b2224e4e762660
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 17:00:40 -0700

    test: add unit test for CategoryController addCategory method

--- src/test/java/org/yearup/controller/CategoryControllerTest.java
+++ src/test/java/org/yearup/controller/CategoryControllerTest.java
@@ -1,32 +1,23 @@
 package org.yearup.controller;
 
 import org.junit.jupiter.api.Test;
+import org.yearup.model.Category;
+import org.yearup.repository.CategoryRepository;
+import org.yearup.repository.ProductRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CategoryControllerTest {
 
     @Test
-    void getAll() {
-    }
-
-    @Test
-    void getById() {
-    }
-
-    @Test
-    void getProductsById() {
-    }
-
-    @Test
     void addCategory() {
-    }
-
-    @Test
-    void updateCategory() {
-    }
-
-    @Test
-    void deleteCategory() {
+        CategoryRepository catRepo = org.mockito.Mockito.mock(CategoryRepository.class);
+        ProductRepository prodRepo = org.mockito.Mockito.mock(ProductRepository.class);
+        CategoryController controller = new CategoryController(catRepo, prodRepo);
+        Category c = new Category();
+        org.mockito.Mockito.when(catRepo.save(c)).thenReturn(c);
+        var response = controller.addCategory(c);
+        org.mockito.Mockito.verify(catRepo).save(c);
+        assertEquals(201, response.getStatusCodeValue());
     }
 }
