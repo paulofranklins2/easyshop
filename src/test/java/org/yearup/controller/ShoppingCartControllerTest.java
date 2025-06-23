4ebab9cd40edae876242fa02e322c77e69a8074b
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 17:03:27 -0700

    test: add unit test for ShoppingCartController getCart method

--- src/test/java/org/yearup/controller/ShoppingCartControllerTest.java
+++ src/test/java/org/yearup/controller/ShoppingCartControllerTest.java
@@ -1,6 +1,10 @@
 package org.yearup.controller;
 
 import org.junit.jupiter.api.Test;
+import org.yearup.model.ShoppingCart;
+import org.yearup.model.User;
+import org.yearup.repository.UserRepository;
+import org.yearup.service.ShoppingCartService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,21 +12,14 @@ class ShoppingCartControllerTest {
 
     @Test
     void getCart() {
-    }
-
-    @Test
-    void addToCart() {
-    }
-
-    @Test
-    void updateCartQuantity() {
-    }
-
-    @Test
-    void deleteProduct() {
-    }
-
-    @Test
-    void clearCart() {
+        ShoppingCartService service = org.mockito.Mockito.mock(ShoppingCartService.class);
+        UserRepository repo = org.mockito.Mockito.mock(UserRepository.class);
+        ShoppingCartController controller = new ShoppingCartController(service, repo);
+        User u = new User();
+        u.setId(1);
+        org.mockito.Mockito.when(repo.findByUsername("user")).thenReturn(u);
+        org.mockito.Mockito.when(service.getCart(1)).thenReturn(new ShoppingCart());
+        java.security.Principal principal = () -> "user";
+        assertNotNull(controller.getCart(principal));
     }
 }
