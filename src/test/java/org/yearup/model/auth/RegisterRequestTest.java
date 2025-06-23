f7c5c175cee1bb72fa6e88a4077ac4c580fc0a30
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 17:03:09 -0700

    test: add unit test for RegisterRequest model getters and setters

--- src/test/java/org/yearup/model/auth/RegisterRequestTest.java
+++ src/test/java/org/yearup/model/auth/RegisterRequestTest.java
@@ -2,39 +2,21 @@ package org.yearup.model.auth;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RegisterRequestTest {
 
     @Test
-    void getUsername() {
-    }
+    void gettersAndSetters() {
+        RegisterRequest req = new RegisterRequest();
+        req.setUsername("bob");
+        req.setPassword("p");
+        req.setConfirmPassword("p");
+        req.setRole("USER");
 
-    @Test
-    void getPassword() {
-    }
-
-    @Test
-    void getConfirmPassword() {
-    }
-
-    @Test
-    void getRole() {
-    }
-
-    @Test
-    void setUsername() {
-    }
-
-    @Test
-    void setPassword() {
-    }
-
-    @Test
-    void setConfirmPassword() {
-    }
-
-    @Test
-    void setRole() {
+        assertEquals("bob", req.getUsername());
+        assertEquals("p", req.getPassword());
+        assertEquals("p", req.getConfirmPassword());
+        assertEquals("USER", req.getRole());
     }
 }
