9667a4da2d3429a07f97cb33b399127b1d78d327
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 17:02:27 -0700

    test: add unit test for ProductController search method

--- src/test/java/org/yearup/controller/ProductControllerTest.java
+++ src/test/java/org/yearup/controller/ProductControllerTest.java
@@ -1,6 +1,7 @@
 package org.yearup.controller;
 
 import org.junit.jupiter.api.Test;
+import org.yearup.repository.ProductRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,21 +9,10 @@ class ProductControllerTest {
 
     @Test
     void search() {
-    }
-
-    @Test
-    void getById() {
-    }
-
-    @Test
-    void addProduct() {
-    }
-
-    @Test
-    void updateProduct() {
-    }
-
-    @Test
-    void deleteProduct() {
+        ProductRepository repo = org.mockito.Mockito.mock(ProductRepository.class);
+        ProductController controller = new ProductController(repo);
+        org.mockito.Mockito.when(repo.search(null, null, null, null, org.springframework.data.domain.PageRequest.of(0,25)))
+            .thenReturn(new org.springframework.data.domain.PageImpl<>(java.util.List.of()));
+        assertTrue(controller.search(null,null,null,null,0,25,null).isEmpty());
     }
 }
