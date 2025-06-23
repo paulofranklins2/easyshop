37a2939040f76c01aadab4df72fc8493935ec323
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 17:04:20 -0700

    test: add unit test for ShoppingCart item management and total calculation

--- src/test/java/org/yearup/model/ShoppingCartTest.java
+++ src/test/java/org/yearup/model/ShoppingCartTest.java
@@ -2,31 +2,23 @@ package org.yearup.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ShoppingCartTest {
 
     @Test
     void contains() {
-    }
+        ShoppingCart cart = new ShoppingCart();
+        ShoppingCartItem item = new ShoppingCartItem();
+        Product p = new Product();
+        p.setProductId(1);
+        p.setPrice(java.math.BigDecimal.TEN);
+        item.setProduct(p);
+        cart.add(item);
 
-    @Test
-    void add() {
-    }
-
-    @Test
-    void get() {
-    }
-
-    @Test
-    void getTotal() {
-    }
-
-    @Test
-    void getItems() {
-    }
-
-    @Test
-    void setItems() {
+        assertTrue(cart.contains(1));
+        assertEquals(item, cart.get(1));
+        assertEquals(java.math.BigDecimal.TEN, cart.getTotal());
     }
 }
