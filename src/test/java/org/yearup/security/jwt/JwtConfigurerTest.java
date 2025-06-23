15aa4b2b7c306e39568c18fd6cffe65cf7dc383b
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 17:11:48 -0700

    test: verify JwtConfigurer adds JwtFilter before UsernamePasswordAuthenticationFilter

--- src/test/java/org/yearup/security/jwt/JwtConfigurerTest.java
+++ src/test/java/org/yearup/security/jwt/JwtConfigurerTest.java
@@ -1,12 +1,29 @@
 package org.yearup.security.jwt;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 class JwtConfigurerTest {
 
     @Test
-    void configure() {
+    void configure_addsJwtFilterBeforeUsernamePasswordAuthenticationFilter() throws Exception {
+        // Arrange
+        TokenProvider provider = mock(TokenProvider.class);
+        JwtConfigurer configurer = new JwtConfigurer(provider);
+
+        HttpSecurity http = mock(HttpSecurity.class); // Needs mockito-inline to work
+        when(http.addFilterBefore(any(JwtFilter.class), eq(UsernamePasswordAuthenticationFilter.class)))
+            .thenReturn(http);
+
+        // Act
+        configurer.configure(http);
+
+        // Assert
+        verify(http).addFilterBefore(any(JwtFilter.class), eq(UsernamePasswordAuthenticationFilter.class));
     }
 }
