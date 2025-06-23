37ded705c93d187579b1478a5b9632dbbb6d344b
paulofranklins <paulofranklinc@gmail.com>
2025-06-23 14:10:07 -0700

    converting layout to mario theme

--- src/main/resources/static/js/services/orders-service.js
+++ src/main/resources/static/js/services/orders-service.js
@@ -10,10 +10,7 @@ class OrdersService {
                     const month = String(date.getMonth() + 1).padStart(2, '0');
                     const day = String(date.getDate()).padStart(2, '0');
                     const year = date.getFullYear();
-                    const hours = String(date.getHours()).padStart(2, '0');
-                    const minutes = String(date.getMinutes()).padStart(2, '0');
-                    const seconds = String(date.getSeconds()).padStart(2, '0');
-                    o.date = `${month}:${day}:${year} ${hours}:${minutes}:${seconds}`;
+                    o.date = `${month}/${day}/${year}`;
                     return o;
                 });
                 const data = {orders: orders, hasOrders: orders.length > 0};
