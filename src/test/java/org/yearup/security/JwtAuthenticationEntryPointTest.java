d9d669e5b55a7d4b1ab3159a070995dd1a786dbc
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 17:11:34 -0700

    test: add unit test for JwtAuthenticationEntryPoint commence method

--- src/test/java/org/yearup/security/JwtAuthenticationEntryPointTest.java
+++ src/test/java/org/yearup/security/JwtAuthenticationEntryPointTest.java
@@ -2,11 +2,18 @@ package org.yearup.security;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JwtAuthenticationEntryPointTest {
 
     @Test
-    void commence() {
+    void commence() throws IOException {
+        JwtAuthenticationEntryPoint entry = new JwtAuthenticationEntryPoint();
+        org.springframework.mock.web.MockHttpServletResponse res = new org.springframework.mock.web.MockHttpServletResponse();
+        entry.commence(new org.springframework.mock.web.MockHttpServletRequest(), res,
+            new org.springframework.security.core.AuthenticationException("err"){});
+        assertEquals(401, res.getStatus());
     }
 }
