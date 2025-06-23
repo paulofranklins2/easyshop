80aa351c510be6a3af959fa9d06022df7bd38a50
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 17:01:39 -0700

    test: add unit test for LoginResponse constructor and getters

--- src/test/java/org/yearup/model/auth/LoginResponseTest.java
+++ src/test/java/org/yearup/model/auth/LoginResponseTest.java
@@ -1,24 +1,17 @@
 package org.yearup.model.auth;
 
 import org.junit.jupiter.api.Test;
+import org.yearup.model.User;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoginResponseTest {
 
     @Test
-    void getToken() {
-    }
-
-    @Test
-    void getUser() {
-    }
-
-    @Test
-    void setToken() {
-    }
-
-    @Test
-    void setUser() {
+    void gettersAndSetters() {
+        User user = new User();
+        LoginResponse res = new LoginResponse("abc", user);
+        assertEquals("abc", res.getToken());
+        assertEquals(user, res.getUser());
     }
 }
