d669b58dc183e92f9c104f2b0de3f93ad2fe6128
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 20:11:03 -0700

    javadoc

--- src/main/java/org/yearup/EasyShopApplication.java
+++ src/main/java/org/yearup/EasyShopApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Application entry point.
+ *
+ * @author Paulo Cunha
+ * @see <a href="https://www.linkedin.com/in/paulofranklins/">LinkedIn</a>
+ * @see <a href="https://github.com/paulofranklins2/">GitHub</a>
  */
+
 @SpringBootApplication
 public class EasyShopApplication {
 
