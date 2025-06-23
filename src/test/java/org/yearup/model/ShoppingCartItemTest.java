12bac62f5df1ff5d72a009a6fcb917af40accec1
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 17:03:59 -0700

    test: add unit test for ShoppingCartItem calculations and product linking

--- src/test/java/org/yearup/model/ShoppingCartItemTest.java
+++ src/test/java/org/yearup/model/ShoppingCartItemTest.java
@@ -2,39 +2,21 @@ package org.yearup.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ShoppingCartItemTest {
 
     @Test
-    void getProductId() {
-    }
+    void calculations() {
+        Product p = new Product();
+        p.setProductId(9);
+        p.setPrice(java.math.BigDecimal.TEN);
+        ShoppingCartItem item = new ShoppingCartItem();
+        item.setProduct(p);
+        item.setQuantity(2);
+        item.setDiscountPercent(new java.math.BigDecimal("0.1"));
 
-    @Test
-    void getLineTotal() {
-    }
-
-    @Test
-    void getProduct() {
-    }
-
-    @Test
-    void getQuantity() {
-    }
-
-    @Test
-    void getDiscountPercent() {
-    }
-
-    @Test
-    void setProduct() {
-    }
-
-    @Test
-    void setQuantity() {
-    }
-
-    @Test
-    void setDiscountPercent() {
+        assertEquals(9, item.getProductId());
+        assertEquals(new java.math.BigDecimal("18.0"), item.getLineTotal());
     }
 }
