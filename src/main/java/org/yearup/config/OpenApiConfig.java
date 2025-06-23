555c4ad133e64722b968f27a7c9eb7e468eb2d17
paulofranklins <paulofranklinc@gmail.com>
2025-06-23 08:18:03 -0700

    feat: implemented bearer token to swagger

--- src/main/java/org/yearup/config/OpenApiConfig.java
+++ src/main/java/org/yearup/config/OpenApiConfig.java
@@ -2,7 +2,10 @@ package org.yearup.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,10 +27,20 @@ public class OpenApiConfig {
      */
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
             .info(new io.swagger.v3.oas.models.info.Info()
                 .title("EasyShop API")
                 .version("v1")
-                .description("REST API documentation for the EasyShop application"));
+                .description("REST API documentation for the EasyShop application"))
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .components(new Components()
+                .addSecuritySchemes(securitySchemeName,
+                    new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
     }
 }
