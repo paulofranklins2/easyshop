8d84f07cbe68a0a4be4380262befbcd418e54b33
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 17:01:02 -0700

    test: add unit test for DatabaseConfig dataSource initialization

--- src/test/java/org/yearup/configuration/DatabaseConfigTest.java
+++ src/test/java/org/yearup/configuration/DatabaseConfigTest.java
@@ -7,5 +7,8 @@ class DatabaseConfigTest {
 
     @Test
     void dataSource() {
+        DatabaseConfig config = new DatabaseConfig("jdbc:h2:mem:test","u","p");
+        assertNotNull(config.dataSource());
+        assertEquals("jdbc:h2:mem:test", config.dataSource().getUrl());
     }
 }
