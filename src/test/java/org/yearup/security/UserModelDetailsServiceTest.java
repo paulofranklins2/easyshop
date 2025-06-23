cc27f856c44220f483aa03e977646989aeb7be3e
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 17:04:50 -0700

    test: add unit test for UserModelDetailsService loadUserByUsername method

--- src/test/java/org/yearup/security/UserModelDetailsServiceTest.java
+++ src/test/java/org/yearup/security/UserModelDetailsServiceTest.java
@@ -1,6 +1,7 @@
 package org.yearup.security;
 
 import org.junit.jupiter.api.Test;
+import org.yearup.model.User;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +9,10 @@ class UserModelDetailsServiceTest {
 
     @Test
     void loadUserByUsername() {
+        org.yearup.repository.UserRepository repo = org.mockito.Mockito.mock(org.yearup.repository.UserRepository.class);
+        org.mockito.Mockito.when(repo.findByUsername("user")).thenReturn(new User("user","p","USER"));
+        UserModelDetailsService svc = new UserModelDetailsService(repo);
+        var details = svc.loadUserByUsername("user");
+        assertEquals("user", details.getUsername());
     }
 }
