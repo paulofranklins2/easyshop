6d02e60304102dcb65eab5b4a0b8f1c6b0b9aebf
paulofranklins <paulofranklinc@gmail.com>
2025-06-22 17:00:26 -0700

    test: add unit test for Authority model behavior

--- src/test/java/org/yearup/model/auth/AuthorityTest.java
+++ src/test/java/org/yearup/model/auth/AuthorityTest.java
@@ -7,18 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class AuthorityTest {
 
     @Test
-    void testEquals() {
-    }
-
-    @Test
-    void getName() {
-    }
-
-    @Test
-    void setName() {
-    }
-
-    @Test
-    void testToString() {
+    void basic() {
+        Authority a1 = new Authority("ROLE_USER");
+        Authority a2 = new Authority("ROLE_USER");
+        assertEquals(a1, a2);
+        a2.setName("ROLE_ADMIN");
+        assertEquals("ROLE_ADMIN", a2.getName());
+        assertTrue(a2.toString().contains("ROLE_ADMIN"));
     }
 }
