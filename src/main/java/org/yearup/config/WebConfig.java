d531d189eaf2e16a8f6d1c0d0a63d813b3a48506
paulofranklins <paulofranklinc@gmail.com>
2025-11-14 22:24:06 -0800

    small fix.

---
+++ src/main/java/org/yearup/config/WebConfig.java
@@ -0,0 +1,16 @@
+package org.yearup.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        // This will add '/api' prefix to all @RequestMapping paths
+        configurer.addPathPrefix("/api", 
+            c -> c.isAnnotationPresent(org.springframework.web.bind.annotation.RestController.class));
+    }
+}
--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -1,3 +1,3 @@
 const config = {
-    baseUrl: 'http://localhost'
+    baseUrl: 'http://localhost/api'
 }
